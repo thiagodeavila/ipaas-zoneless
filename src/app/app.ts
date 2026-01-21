543026c86906a36d0f668358d77132bd2e9b9e66
Thiago de Avila <t.avila@totvs.com.br>
2026-01-21 14:34:56 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EFMarkerType, FCanvasComponent, FCreateConnectionEvent, FFlowModule, FZoomDirective, IFFlowStateConnection, IFFlowStateNode } from '@foblex/flow';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FFlowModule, FZoomDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('zoneless-app');
+
+  @ViewChild(FCanvasComponent, { static: true })
+  public fCanvas!: FCanvasComponent;
+
+  public connections: { from: string, to: string }[] = [];
+
+  public eMarkerType = EFMarkerType;
+
+  viewModel = signal<{ nodes: IFFlowStateNode[], connections: IFFlowStateConnection[] } | null>(null);
+
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+  public onLoaded(): void {
+    this.fCanvas.resetScaleAndCenter(false);
+  }
+
+  public onCreateConnection(event: FCreateConnectionEvent): void {
+    if (!event.fInputId) {
+      return;
+    }
+    this.connections.push({ from: event.fOutputId, to: event.fInputId });
+  }
+
+  public onDeleteConnections(): void {
+    this.connections = [];
+    this.changeDetectorRef.detectChanges();
+  }
 }
